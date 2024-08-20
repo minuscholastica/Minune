f8943d4f7b4a728edd031f271ecbb7f412b524f2
Minu <minuscholastica0209@gmail.com>
2024-08-21 03:48:29 +0800

    Sitemap, build error

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://minuchoi.com',
+  generateRobotsTxt: true,
+  // Additional options can be added here if needed
+};
+
+module.exports = config;
