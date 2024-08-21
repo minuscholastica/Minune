7d27f2ad8b35c7e6a80172368e7c60b870599f73
Minu <minuscholastica0209@gmail.com>
2024-08-22 03:36:48 +0800

    CalloutBox Style

--- components/CalloutBox.tsx
+++ components/CalloutBox.tsx
@@ -9,13 +9,13 @@ interface CalloutBoxProps {
 
 const CalloutBox: React.FC<CalloutBoxProps> = ({ children, icon }) => {
   return (
-    <div className="bg-[#e8f0fe] rounded-lg p-4 my-4 flex">
+    <div className="bg-[#d0e3ff] rounded-lg p-4 my-4 flex items-start shadow-sm">
       {icon && (
-        <div className="mr-4">
+        <div className="mr-4 text-[#4a5568]">
           <span className="text-2xl">{icon}</span>
         </div>
       )}
-      <div className="flex-grow">{children}</div>
+      <div className="flex-grow text-[#2d3748]">{children}</div>
     </div>
   );
 };
