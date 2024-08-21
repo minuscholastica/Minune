bee3bebb9de3080bcf6a1891d4ecae965be322f3
Minu <minuscholastica0209@gmail.com>
2024-08-21 12:32:08 +0800

    meta icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,19 @@ import ClientLayout from './ClientLayout'
 export const metadata: Metadata = {
   title: 'Minune',
   description: 'Untimely Meditations at Minune',
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
