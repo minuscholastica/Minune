696805f3b830ea7c35b96f7dea065e3d745cb8d3
Minu <minuscholastica0209@gmail.com>
2024-08-22 04:18:01 +0800

    Adjusted margins

--- components/ContentRenderer.tsx
+++ components/ContentRenderer.tsx
@@ -10,8 +10,39 @@ interface ContentRendererProps {
 }
 
 const ContentRenderer: React.FC<ContentRendererProps> = ({ content }) => {
-  const renderContent = () => {
-    // ... (previous renderContent logic remains the same)
+  const renderContent = (): JSX.Element[] => {
+    const parts: JSX.Element[] = [];
+    let lastIndex = 0;
+    const regex = /<CalloutBox.*?>([\s\S]*?)<\/CalloutBox>/g;
+    let match;
+
+    while ((match = regex.exec(content)) !== null) {
+      if (match.index > lastIndex) {
+        parts.push(
+          <div key={lastIndex} dangerouslySetInnerHTML={{ __html: content.slice(lastIndex, match.index) }} />
+        );
+      }
+
+      const calloutContent = match[1];
+      const headingMatch = match[0].match(/heading="([^"]*)"/);
+      const heading = headingMatch ? headingMatch[1] : undefined;
+
+      parts.push(
+        <CalloutBox key={match.index} heading={heading}>
+          <div dangerouslySetInnerHTML={{ __html: calloutContent }} />
+        </CalloutBox>
+      );
+
+      lastIndex = regex.lastIndex;
+    }
+
+    if (lastIndex < content.length) {
+      parts.push(
+        <div key={lastIndex} dangerouslySetInnerHTML={{ __html: content.slice(lastIndex) }} />
+      );
+    }
+
+    return parts;
   };
 
   return (
